93a23a25bc65078c4958dcfa952b37843bfe07f9
GabielFemi <gabrielfemi799@gmail.com>
2020-05-26 12:58:10 +0200

    read quiz using a separate package - gabby_csv.ReadCsv

--- quiz/quiz.go
+++ quiz/quiz.go
@@ -1,7 +1,33 @@
 package quiz
 
-import gabby_csv "github.com/gabielfemi/csvManip/csv"
+import (
+	"encoding/csv"
+	gabby_csv "github.com/gabielfemi/csvManip/csv"
+	"log"
+	"os"
+)
 
 func ReadQuizFromCsv() {
-	gabby_csv.ReadCsv("quiz.csv")
+	csvFile := "quiz.csv"
+	gabby_csv.ReadCsv(csvFile)
+	StartReading(csvFile)
+}
+
+func StartReading(csvFile string) {
+	file, err := os.Open(csvFile)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	reader := csv.NewReader(file)
+	reader.FieldsPerRecord = 2
+	reader.Comma = ';'
+
+	csvData, err := reader.ReadAll()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	for _, field := range csvData {
+
+	}
 }
