1cdc18a8999d3378e2a65b7e2d3c9f4cbbd95dbd
GabielFemi <gabrielfemi799@gmail.com>
2020-05-26 14:07:55 +0200

    add function to read input from user using bufio

--- quiz/format.go
+++ quiz/format.go
@@ -1,10 +1,25 @@
 package quiz
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func format(slice []Quiz) {
-	for i := range slice {
-		fmt.Println(slice[i])
+	for quiz := range slice {
+
+		question := slice[quiz].Question
+		answer := slice[quiz].Answer
+		fmt.Println()
+		fmt.Print(question, " ")
+		inputReader := bufio.NewReader(os.Stdin)
+		input, err := inputReader.ReadString('\n')
+
+		if err == nil {
+			fmt.Printf("Your answer was: %s", input)
+			fmt.Printf("The real answer is: %s\n", answer)
+		}
 	}
 
 }
